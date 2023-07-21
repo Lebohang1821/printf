020b9ed3ee13e7313786980b5ddb458eb67f4f8c
Motekema <motekema@gmail.com>
2023-07-21 05:18:55 -0700

    My commit

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ len4 = printf("Let's try to printf a simple sentence.\n");
 ui = (unsigned int)INT_MAX + 1024;
 addr = (void *)0x7ffe637541f0;
 _printf("Length:[%d, %i]\n", len, len);
-printf("Length:[%d, %i]\n", len4, len4); // Note: corrected len2 to len4 here
+printf("Length:[%d, %i]\n", len4, len4); /* Note: corrected len2 to len4 here */
 _printf("Negative:[%d]\n", -762534);
 printf("Negative:[%d]\n", -762534);
 _printf("Unsigned:[%u]\n", ui);
@@ -34,8 +34,8 @@ printf("String:[%s]\n", "I am a string !");
 _printf("Address:[%p]\n", addr);
 printf("Address:[%p]\n", addr);
 len = _printf("Percent:[%%]\n");
-len4 = printf("Percent:[%%]\n"); // Note: corrected len2 to len4 here
+len4 = printf("Percent:[%%]\n"); /* Note: corrected len2 to len4 here */
 _printf("Len:[%d]\n", len);
-printf("Len:[%d]\n", len4); // Note: corrected len2 to len4 here
+printf("Len:[%d]\n", len4); /* Note: corrected len2 to len4 here */
 return (0);
 }
